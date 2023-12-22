d36bc5079b805f32a9a6027c312f3c90634e7f4c
oswald884 <dr-fa_tes@hotmail.com>
2023-12-22 23:18:40 +0300

    style: for blog preview card

--- src/Components/BlogPreviewCard.tsx
+++ src/Components/BlogPreviewCard.tsx
@@ -4,19 +4,19 @@ type Props = {
 
 const BlogPreviewCard = (props: Props) => {
    return (
-      <div>
-         <img src="" alt="" />
-         <span>Learning</span>
-         <span>Published 21 Dec 2023</span>
-         <h3>HTML & CSS foundations</h3>
-         <p>
+      <div className="blog-preview-card col-3">
+         <img className="illustration" src="./images/illustration-article.svg" alt="blog cover image" />
+         <span className="pill">Learning</span>
+         <span className="publish-date">Published 21 Dec 2023</span>
+         <h2 className="title">HTML & CSS foundations</h2>
+         <p className="description">
             These languages are the backbone of every
             website, defining structure, content and
             presentation.
          </p>
-         <div>
-            <img src="" alt="" />
-            <span>Grag Hooper</span>
+         <div className="author-container">
+            <img src="./images/image-avatar.webp" alt="author image" />
+            <span>Greg Hooper</span>
          </div>
       </div>
    );
